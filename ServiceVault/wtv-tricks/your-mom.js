0e37bc2e86102058350eb0df420d05c29ab9f68c
Dylan Funderburg <hiden64@protonmail.com>
2024-07-12 23:54:49 -0500

    :trol:

---
+++ ServiceVault/wtv-tricks/your-mom.js
@@ -0,0 +1,17 @@
+var minisrv_service_file = true;
+
+headers = `200 OK
+Content-Type: text/html`;
+
+data = `<html>
+<head>
+<title>your mom</title>
+</head>
+<body>
+<center>
+<hr>
+<font size=3><b><p>This page is too big to be shown completely.</p></b></font>
+</center>
+</body>
+</html>
+`;
