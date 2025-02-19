20a4c474441c37a47b6c734d12915395795e1a16
Dylan Funderburg <hiden64@protonmail.com>
2025-02-18 20:35:49 -0600

    cult of webtv routine ceremony
    (club webtv post)

---
+++ ServiceVault/wtv-center/cwtv/2025-02/update.js
@@ -0,0 +1,152 @@
+var minisrv_service_file = true;
+
+headers = `200 OK
+Connection: Keep-Alive
+Content-Type: text/html`
+
+
+data = `<html><head>
+<title>
+Club WebTV: Back to the Past
+</title>
+</head><body fontsize="medium" vspace="0" hspace="0" vlink="#ABA6EF" text="#dbdbd1" link="#189BD5" bgcolor="#171F30" alink="#ABA6EF"><display>
+<style><!-- A{text-decoration:none} no underline --></style>
+
+
+
+<sidebar width="152" text="#E6E8AF" link="#E6E8AF" vlink="#E6E8AF" alink="#E6E8AF" fontsize="medium">
+
+<table width="152" cellspacing="0" cellpadding="0" border="0" align="left">
+<!--LOGO BOX-->
+  <tbody><tr>
+    <td><table absheight="85" width="152" cellspacing="0" cellpadding="0" border="0" bgcolor="#101821" background="wtv-center:/cwtv/images/founders_left_green.jpg">      <tbody><tr>        <td width="6"></td>
+        <td width="140" align="center"><table width="140" cellspacing="0" cellpadding="0">
+          <tbody><tr>            <td height="6"></td>          </tr>          <tr>
+		  
+            <td href="wtv-home:/home" width="140" height="73" align="center"><img src="wtv-home:/ROMCache/WebTVLogoJewel.gif" width="87" height="67"></td>
+          </tr>          <tr>            <td height="6"></td>          </tr>
+        </tbody></table>        </td>        <td width="6"></td>      </tr>    </tbody></table>
+    </td>  </tr>
+
+		
+<!--TABLE OF CONTENTS-->  <tr>
+    <td><table cellspacing="0" cellpadding="0" border="0" bgcolor="#101821">
+      <tbody><tr>
+        <td colspan="3" height="9"></td>
+      </tr>
+      <tr>
+        <td width="6" valign="top"></td>
+
+
+
+        <td width="140" valign="top" align="left"><table width="100%" cellspacing="0" cellpadding="3" border="0"><tbody>
+		<tr><td><!--<a href="archive.html"><font size="+1" color="#E6E8AF"><blackface>Home</blackface></font></a>--></td></tr>
+		<tr><td><!--<a href="archive.html"><font size="+1" color="#E6E8AF"><blackface>Archive</blackface></font></a>--></td></tr></tbody></table></td>
+
+
+
+
+        <td abswidth="6"><img src="wtv-home:/ROMCache/spacer.gif" width="1" height="299"></td>
+      </tr>
+    </tbody></table>
+    </td>
+  </tr></tbody></table></sidebar>
+
+
+<table cellspacing="0" cellpadding="0" border="0" align="left">  <tbody><tr>    <td><table width="408" height="85" cellspacing="0" cellpadding="0" border="0" background="wtv-center:/cwtv/images/section_title_green.jpg">      <tbody><tr>
+        <td absheight="85" colspan="1" valign="top"><spacer type="block" width="408" height="14"><br clear="all">
+<spacer type="block" width="16" height="66" align="left"><font size="+3" color="#E6CD4A"><blackface><shadow>Latest news</shadow></blackface></font>
+
+</spacer></spacer></td>
+      </tr>
+    </tbody></table>
+    </td>
+  </tr>
+  <tr>
+    <td><table width="408" cellspacing="0" cellpadding="0" border="0">
+      <tbody><tr>
+        <td colspan="3" height="18"></td>
+      </tr>
+      <tr>
+        <td abswidth="40"></td>
+        <td valign="top">
+<b><font size="+5" color="#C0F9A3">Back to the Past</font><font size="5" color="#C0F9A3">
+</font></b>
+
+<br>
+<br>
+<font size="4">
+
+<p>
+
+<i>To the 90s, specifically.</i>
+</p>
+<p><font size="3" color="#FFFFFF">By: <i><b>HIDEN</b></i></font></p><br><p>
+Hey all! We've introduced some new features and other improvements to the service in the past few months, for nostalgia junkies and general WebTV fanatics alike. If you've missed out on the news, then just simply read below.
+</p><br><p>
+<blackface>Back to '97</blackface>
+</p><p>
+You now have the option of enabling the classic home page (the design from before 1999) in Setup. Time travel back to the past, as we are doing with this page's design!
+</p><p>
+<blackface>Messenger Setup UX improvements</blackface
+</p><p>
+We've added a number of improvements to the Messenger Setup pages, because they were a bit too confusing: Setting account details and setting a server is now combined into one page, and for CrossTalk, setting an e-mail address now only involves one text prompt, where, as you'd expect, enter your e-mail address as normal. Crazy, right?
+</p><p>
+We've also lowered the minimum length for Messenger passwords to 6 characters, removed the password requirement for Tricks (but added some mild idiot-proofing to compensate), and more. We've certainly got even more on the way, too. Stay tuned! ;)
+</p><br><p>
+<b>Known Issue</b>: We are aware of the issues with the ProtoWeb proxy, and we've believed to have narrowed it down to a DNS-related issue. Hopefully a fix will hopefully be coming soon. In the meantime, expect the ProtoWeb proxy's functionality to be hit or miss.
+</p><br><p>
+The next Club WebTV issue will feature community submitted web sites and questions. We didn't get enough for them to be featured this time around. If you wish to submit a question or a Page Builder web site to show off, send a message to <a href="mailto:ClubWebTV@WebTV">ClubWebTV@WebTV</a>.
+</p><p>
+Have any further questions? Need help? Just want to say hi and pester me? You can shoot me a message at <a href="mailto:hiden64@WebTV">hiden64@WebTV</a>, and I'll try and respond to every message.
+</p><p>
+</p></font><p><font size="4">
+Now, go and surf that darn web!
+
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+</font>
+
+            </p>
+          
+        
+        </td>
+        <td abswidth="20"><img src="wtv-home:/ROMCache/spacer.gif" width="26" height="1"></td>
+      </tr>
+      <tr>
+        <td colspan="3" height="20"></td>
+      </tr>
+    </tbody></table>
+    </td>
+  </tr>
+</tbody></table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</display></body></html>
+`;
