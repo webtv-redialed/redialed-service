b6db61193a9bdf60ae77ce9b658254e8f2a076a0
SK <55817746+SKCro@users.noreply.github.com>
2025-06-27 01:28:57 -0400

    Overhaul splash system
    this took me way too long to do lmfao
    Rewrote splash screen code to allow for new splash logos for occasions like pride month and Christmas. Huge thanks to @JarHead4 for cleaning up my messy code and making it actually decent.

--- ServiceVault/wtv-setup/validate-home-options.js
+++ ServiceVault/wtv-setup/validate-home-options.js
@@ -5,10 +5,12 @@ if (errpage) {
     headers = errpage[0];
     data = errpage[1];
 } else {
-    session_data.setSessionData("alt_home", request_headers.query.home);
+    session_data.setSessionData('alt_home', request_headers.query.home);
+    session_data.setSessionData('splash', request_headers.query.splash);
     session_data.saveSessionData;
     headers = `302 Moved temporarily
 wtv-expire-all: wtv-home:/home
+wtv-expire-all: wtv-home:/splash
 wtv-expire-all: wtv-setup:/home
 Location: wtv-setup:/setup
 `;
