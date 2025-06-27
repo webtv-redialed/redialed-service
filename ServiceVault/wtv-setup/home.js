735c2bf4260034015391788f145f4a6f469a16cd
SK <55817746+SKCro@users.noreply.github.com>
2025-06-27 01:28:57 -0400

    Overhaul splash system
    this took me way too long to do lmfao
    Rewrote splash screen code to allow for new splash logos for occasions like pride month and Christmas. Huge thanks to @JarHead4 for cleaning up my messy code and making it actually decent.

--- ServiceVault/wtv-setup/home.js
+++ ServiceVault/wtv-setup/home.js
@@ -4,59 +4,78 @@ headers = `200 OK
 Connection: Keep-Alive
 Content-Type: text/html`;
 
-const nunjucks = require("nunjucks");
+const nunjucks = require('nunjucks');
 nunjucks.configure({ autoescape: false });
 
 // this sucks
 let sitetitle = ''
 
 if (session_data.hasCap("client-has-tv-experience"))
-    sitetitle = 'Web Home options'
+		sitetitle = 'Web Home options'
 else
-    sitetitle = 'Home options'
+		sitetitle = 'Home options'
 
 data = nunjucks.render('ServiceDeps/templates/wtv-setup/setupGeneral.njk', { title: sitetitle, body: `<td abswidth=20>
-<TR>
-<td>
-<td WIDTH=215 HEIGHT=236 VALIGN=top ALIGN=left>
-<p>Turn ${session_data.getSessionData("alt_home") == '1' ? "off" : "on"} the classic ${session_data.hasCap("client-has-tv-experience") ? `Web Home` : 'Home'} page design.
-<TD WIDTH=20>
-<TD WIDTH=198 VALIGN=top ALIGN=left>
+<tr><td>
+<td width=215 height=236 Valign=top align=left>
+<p>Use these options to customize how WebTV's home page looks and acts.
+<td width=20>
+<td width=198 Valign=top align=left>
 <form action="wtv-setup:/validate-home-options">
 <table cellspacing=0 cellpadding=0>
-<tr>
-<td valign=top>
-<INPUT TYPE="checkbox" NAME="home" VALUE="1" ${session_data.getSessionData("alt_home") == '1' ? "checked" : ""}>
+<tr><td valign=middle>
+<input type=checkbox name=home value=1${session_data.getSessionData('alt_home') == '1' ? ' checked' : ''}>
 <td abswidth=4>
-<td valign=top>
-<font size=-1>Classic home</font>
+<td valign=middle>
+<font size=-1>Classic home <a href="client:showalert?message=Enable this option to use the classic 1998 home page design rather than the current one.&buttonlabel1=Dismiss&buttonaction1=client:doNothing&buttonlabel2=Preview&buttonaction2=wtv-home:/home?force_old_home%3Dtrue"><img src=images/HelpButton.gif width=20 height=20 align=absbottom></a></font>
 <tr><td absheight=10>
-<tr>
-<td valign=top>
+<tr><td valign=top>
 </table>
-<TR>
-<TD>
-<TD COLSPAN=4 HEIGHT=14 VALIGN=top ALIGN=left>
-<tr>
-<TD>
-<td colspan=4 height=2 valign=middle align=center bgcolor="2B2B2B">
+<table cellspacing=0 cellpadding=0>
+<tr><td valign=top>
+<script>function preview(){switch(document.forms[0].splash.selectedIndex){
+case 0:location.href='client:showSplash?message=<font size%3D1>Let WebTV choose a splash logo.&action=client:doNothing&image=wtv-setup:/images/SplashPreviewAuto.gif'
+break
+case 1:location.href='client:showSplash?message=<font size%3D1>A splash logo with a blue background.&action=client:doNothing&image=wtv-setup:/images/SplashPreviewSKCro.gif'
+break
+case 2:location.href='client:showSplash?message=<font size%3D1>A pride month splash logo.&action=client:doNothing&image=wtv-setup:/images/SplashPreviewPride.gif'
+break
+case 3:location.href='client:showSplash?message=<font size%3D1>A darker pride month splash logo.&action=client:doNothing&image=wtv-setup:/images/SplashPreviewMulticolor.gif'
+break
+case 4:location.href='client:showSplash?message=<font size%3D1>A brighter pride month splash logo.&action=client:doNothing&image=wtv-setup:/images/SplashPreviewSupergay.gif'
+break
+case 5:location.href='client:showSplash?message=<font size%3D1>joeb&action=client:doNothing&image=wtv-setup:/images/SplashPreviewJoeb.gif'
+break
+default:return;
+}}</script>
+<font size=-1>Splash logo: <a href="client:showalert?message=Use the dropdown menu to change the splash logo that appears when you log into WebTV.&buttonlabel1=Dismiss&buttonaction1=client:doNothing"><img src=images/HelpButton.gif width=20 height=20 align=absbottom></a>
+<spacer type=vertical height=48><font size=-2><select name=splash usestyle insetselection onchange="preview();return false">
+<option value=auto${session_data.getSessionData('splash') == 'auto' || !session_data.getSessionData('splash') ? ' selected' : ''}>Automatic</option>
+<option value=SKCro${session_data.getSessionData('splash') == 'SKCro' ? ' selected' : ''}>SKCro Blue</option>
+<option value=pride${session_data.getSessionData('splash') == 'pride' ? ' selected' : ''}>Pride Month</option>
+<option value=multicolor${session_data.getSessionData('splash') == 'multicolor' ? ' selected' : ''}>Pride Month Dark</option>
+<option value=supergay${session_data.getSessionData('splash') == 'supergay' ? ' selected' : ''}>Pride Month Bright</option>
+<option value=joeb${session_data.getSessionData('splash') == 'joeb' ? ' selected' : ''}>joeb</option>
+</select></font></font>
+<tr><td absheight=10>
+<tr><td valign=top>
+</table>
+<tr><td>
+<td colspan=4 height=14 Valign=top align=left>
+<tr><td>
+<td colspan=4 height=2 valign=middle align=center bgcolor=2b2b2b>
 <spacer type=block width=436 height=1>
-<tr>
-<TD>
+<tr><td>
 <td colspan=4 height=1 valign=top align=left>
-<tr>
-<TD>
-<td colspan=4 height=2 valign=top align=left bgcolor="0D0D0D">
+<tr><td>
+<td colspan=4 height=2 valign=top align=left bgcolor=0d0d0d>
 <spacer type=block width=436 height=1>
-<TR>
-<TD>
-<TD COLSPAN=4 HEIGHT=4 VALIGN=top ALIGN=left>
-<TR>
-<TD>
-<TD COLSPAN=3 VALIGN=top ALIGN=right>
-<FONT COLOR="#E7CE4A" SIZE=-1><SHADOW>
-<INPUT name="Done" value="Done" WIDTH=103
-TYPE=SUBMIT BORDERIMAGE="file://ROM/Borders/ButtonBorder2.bif" NAME="Button2" USESTYLE WIDTH=103>
-</SHADOW></FONT></FORM>
-<TD>
-</TABLE>`, hasTuner: session_data.hasCap("client-has-tuner"), isJapaneseClient: session_data.isJapaneseClient()});
+<tr><td>
+<td colspan=4 height=4 Valign=top align=left>
+<tr><td>
+<td colspan=3 Valign=top align=right>
+<font color=e7ce4a size=-1 effect=shadow>
+<input type=submit name=Done value=Done width=103 borderimage=file://ROM/Borders/ButtonBorder2.bif name=Button2 usestyle width=103>
+</font></form>
+<td>
+</table>`, hasTuner: session_data.hasCap('client-has-tuner'), isJapaneseClient: session_data.isJapaneseClient()});
