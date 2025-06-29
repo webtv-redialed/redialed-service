5b48a6996a15a795e2a9b30f3ad534934c76dc08
SK <55817746+SKCro@users.noreply.github.com>
2025-06-28 21:07:16 -0400

    Overhaul classic home page
    Reworked the classic home page layout a bit to retrofit the 1999 ads into it, as well as optimize some of the code and make it a bit more consistent so it can be compressed and read better (for example, one-lining some functions, formatting some of the JS in a more readable way, and changing instances of ALLCAPS to lowercase).

--- ServiceVault/wtv-center/templates/en-US/CustomInfoLoadingGrunge.tmpl.js
+++ ServiceVault/wtv-center/templates/en-US/CustomInfoLoadingGrunge.tmpl.js
@@ -8,51 +8,30 @@ const clubWebTVTitle = fs.readFileSync(
 headers = `200 OK`
 
 data = `<html>
-		<head>
-			<display info=WebTVToday teaser1=WebTVToday>
-		</head>
-	
-		<body background="wtv-content:/ROMCache/BackgroundCorner.gif" BGCOLOR=949484
-			onLoad="Initialize()""
-		
+		<head><display info=WebTVToday teaser1=WebTVToday></head>
+		<body background="wtv-content:/ROMCache/BackgroundCorner.gif" bgcolor=949484 onLoad="Initialize()">
 			<table cellspacing=0 cellpadding=0 width=357 height=193 background="wtv-content:/ROMCache/BackgroundWebTVToday_a.swf" valign=top novtilebg>
-			<tr>
-				<td colspan=2 width=357 height=44>
-					<spacer>
+			<tr><td colspan=2 width=357 height=44><spacer>
+			<tr><td width=12><spacer>
+				<td valign=top align=left width=361 height=149><spacer><table cellspacing=0 cellpadding=0>
+			<tr height=40><td width=88><spacer type=block width=88 height=40>
+				<td width=263>
+				<td width=10>
 
-			<tr>
-				<td width=12>
-					<spacer>
-
-				<td valign=TOP align=LEFT width=361 height=149>
-					<spacer><table cellspacing=0 cellpadding=0>
-
-					<tr height=40>
-		<td width=88>
-			<spacer type=block width=88 height=40>
-		<td width=263>
-		<td width=10>
-
-	<tr height=60>
-		<td width=88>
-			<spacer type=block width=88 height=60>
-		<td maxlines=2 valign=top align=left width=263>
-			<table cellspacing=0 cellpadding=0>
-				<tr>
-					<td valign=top maxlines=2>
-						<font size=+2 color="#000000"><b>${clubWebTVTitle}</b></font>
+		<tr height=60>
+			<td width=88><spacer type=block width=88 height=60>
+			<td maxlines=2 valign=top align=left width=263>
+				<table cellspacing=0 cellpadding=0>
+					<tr><td valign=top maxlines=2><font size=+2 color=0><b>${clubWebTVTitle}</b></font>
+				</table>
+			<td width=10>
+		<tr height=49>
+			<td width=88>
+			<spacer type=block width=88 height=49>
+			<td width=263 valign=middle align=right>
+				<font size=-1 color=563333><b>find out more in WebTV Today</b></font>
+			<td width=10>
 			</table>
-		<td width=10>
-
-	<tr height=49>
-
-		<td width=88>
-		<spacer type=block width=88 height=49>
-		<td width=263 valign=middle align=right>
-			<font size=-1 color=#563333><B>find out more in WebTV Today</B></FONT>
-		<td width=10>
-
-			</table>
-			</table>
-		</body>
-	</html>`
+		</table>
+	</body>
+</html>`
